d7ba1dee080b929899eff3c526940a04b1be6b96
Yashima5 <elven.mart@bk.ru>
2022-05-31 10:39:16 +0300

    Project

---
+++ app/src/main/java/com/example/kpkstudentlifeproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.kpkstudentlifeproject;
+
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+private Button scheduleOfClasses;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/kpkstudentlifeproject/ScheduleOfClassesActivity.java
@@ -0,0 +1,13 @@
+package com.example.kpkstudentlifeproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScheduleOfClassesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule_of_classes);
+    }
+}
